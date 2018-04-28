0c707431dc2609cddaaf2d4fac70a7e66898c478
Leonardo Cárdenas <leocardenas2898@gmail.com>
2018-04-28 05:05:29 -0500

    taller 01

--- src/resolucion/EjercicioJ.java
+++ src/resolucion/EjercicioJ.java
@@ -1,20 +1,23 @@
 package resolucion;
 
+import java.util.Random;
 import java.util.Scanner;
-import java.util.Vector;
 
-public class EjercicioI {
+public class EjercicioJ {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner console = new Scanner(System.in);
 		System.out.print("Ingrese el tamaño del vector: ");
 		int n = console.nextInt();
-		Vector<Integer> vector = new Vector<Integer>(n);
-		vector.addElement(2);
-		vector.addElement(2);
-		vector.addElement(2);
-		System.out.println(vector.size());
+		int [] Vector = new int[n];
+		for(int i=0; i<n; i++) {
+			Vector[i]=(int)(Math.random()*10);
+		}
+		
+		for(int i=0; i<n; i++) {
+			System.out.println(Vector[i]);
+		}
 	}
 
 }

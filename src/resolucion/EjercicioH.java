4b3c367b90135295516bf9360d461e67405f7e29
Leonardo Cárdenas <leocardenas2898@gmail.com>
2018-04-28 03:36:02 -0500

    taller 01

--- src/resolucion/EjercicioH.java
+++ src/resolucion/EjercicioH.java
@@ -1,10 +1,30 @@
 package resolucion;
 
+import java.util.Scanner;
+
 public class EjercicioH {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int cantidad, ceros;
+		int i=0;
+		Scanner console = new Scanner(System.in);
+		System.out.print("¿De cuantas filas desea el patron de * ?:  ");
+		cantidad = console.nextInt();
+		if(cantidad==1) {
+			System.out.println("*");
+		}else {
+			System.out.println("*");
+			while(i<cantidad-1) {
+				for(ceros=1; ceros<=i+1;ceros++) {
+					System.out.print("*");
+					if(i==ceros-1) {
+						System.out.println("*");
+					}
+				}
+				i++;
+			}		
+		}		
 	}
-
+	
 }
---
+++ src/resolucion/EjercicioI.java
@@ -0,0 +1,10 @@
+package resolucion;
+
+public class EjercicioI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

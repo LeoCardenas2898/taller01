210ed16f1dc2d9354e6eaffd7f6d45408fcb4b61
Leonardo Cárdenas <leocardenas2898@gmail.com>
2018-04-29 22:58:27 -0500

    taller 01

--- src/resolucion/EjercicioC.java
+++ src/resolucion/EjercicioC.java
@@ -4,10 +4,14 @@ import java.util.Scanner;
 
 public class EjercicioC {
 	
-	public static int mcm(int a, int b) {
-		int resultado = 0;
-		
-		return resultado;
+	public static int mcd(int numero1, int numero2) {
+		if(numero2==0) {
+			return numero1;
+		}else if(numero2>numero1) {
+			return mcd(numero2, numero1);
+		}else {
+			return mcd(numero2,numero1%numero2);
+		}
 	}
 	
 	public static void main(String[] args) {
@@ -16,8 +20,6 @@ public class EjercicioC {
 		int a = console.nextInt();
 		System.out.print("Ingrese el segundo numero: ");
 		int b = console.nextInt();
-		System.out.println("El MCM de "+a+" y "+b+" es "+mcm(a, b));
-		
+		System.out.println((a*b)/mcd(a, b));
 	}
-
 }

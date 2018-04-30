9672a1102b658c4018972bf37a154612b2792187
Leonardo Cárdenas <leocardenas2898@gmail.com>
2018-04-30 00:01:17 -0500

    taller 01

--- src/resolucion/TrabajadorUNTELS.java
+++ src/resolucion/TrabajadorUNTELS.java
@@ -1,5 +1,11 @@
 package resolucion;
 
 public class TrabajadorUNTELS extends Persona{
+	private String areaTrabajo, horario;
+	private int fechaIngreso;
+	
+	public TrabajadorUNTELS() {
+		
+	}
 
 }

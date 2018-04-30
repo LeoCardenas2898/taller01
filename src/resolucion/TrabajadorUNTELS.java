32e8a682aeed6d0da065d75be6ca4ced53513f1f
Leonardo Cárdenas <leocardenas2898@gmail.com>
2018-04-30 00:08:56 -0500

    taller 01

--- src/resolucion/TrabajadorUNTELS.java
+++ src/resolucion/TrabajadorUNTELS.java
@@ -1,20 +1,69 @@
 package resolucion;
 
 public class TrabajadorUNTELS extends Persona{
-	private String areaTrabajo, horario;
-	private int fechaIngreso;
+	private String areaTrabajo, horarioEntrada, horarioSalida;
+	private int fechaIngreso, horasExtras;
+	private Boolean seguro;
 	
 	public TrabajadorUNTELS() {
 		
 	}
 
-	public TrabajadorUNTELS(String areaTrabajo, String horario, int fechaIngreso) {
-		super();
+	public TrabajadorUNTELS(String areaTrabajo, String horarioEntrada, String horarioSalida, int fechaIngreso, int horasExtras, Boolean seguro) {
 		this.areaTrabajo = areaTrabajo;
-		this.horario = horario;
+		this.horarioEntrada = horarioEntrada;
+		this.horarioSalida = horarioSalida;
+		this.fechaIngreso = fechaIngreso;
+		this.horasExtras = horasExtras;
+		this.seguro = seguro;
+	}
+
+	public String getAreaTrabajo() {
+		return areaTrabajo;
+	}
+
+	public void setAreaTrabajo(String areaTrabajo) {
+		this.areaTrabajo = areaTrabajo;
+	}
+
+	public String getHorarioEntrada() {
+		return horarioEntrada;
+	}
+
+	public void setHorarioEntrada(String horarioEntrada) {
+		this.horarioEntrada = horarioEntrada;
+	}
+
+	public String getHorarioSalida() {
+		return horarioSalida;
+	}
+
+	public void setHorarioSalida(String horarioSalida) {
+		this.horarioSalida = horarioSalida;
+	}
+
+	public int getFechaIngreso() {
+		return fechaIngreso;
+	}
+
+	public void setFechaIngreso(int fechaIngreso) {
 		this.fechaIngreso = fechaIngreso;
 	}
-	
-	
+
+	public int getHorasExtras() {
+		return horasExtras;
+	}
+
+	public void setHorasExtras(int horasExtras) {
+		this.horasExtras = horasExtras;
+	}
+
+	public Boolean getSeguro() {
+		return seguro;
+	}
+
+	public void setSeguro(Boolean seguro) {
+		this.seguro = seguro;
+	}
 
 }
